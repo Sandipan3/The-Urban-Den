c1eac6c3778ededb66f542a4a6907eea408712aa
Sandipan Jha <sandipanjha3@gmail.com>
2026-04-20 00:33:32 +0530

    Fix: Images moved to public

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,16 @@ import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import ThemeToggle from "./ThemeToggle";
 
-import logoLight from "../assets/logo-light.svg";
-import logoDark from "../assets/logo-dark.svg";
-
 const Header = ({ dark, toggleTheme }) => {
   return (
     <header className="bg-brand-gray border-b border-black/10 relative">
       <div className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
         <Link to="/" className="flex items-center gap-3">
           <img
-            src={dark ? logoDark : logoLight}
+            src={dark ? "/logo-dark.svg" : "/logo-light.svg"}
             alt="Urban Den Logo"
             className="w-8 h-8"
+            loading="lazy"
           />
 
           <div className="flex flex-col leading-tight">
