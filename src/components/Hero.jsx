daefb902165cedde8e18630e008a0fb8b8fa7db9
Sandipan Jha <sandipanjha3@gmail.com>
2026-04-27 20:29:58 +0530

    Feat: Hero Page added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,61 @@
+import darkLogo from "/logo-dark.svg";
+import LightLogo from "/logo-light.svg";
+
+const Hero = () => {
+  const theme = localStorage.getItem("theme");
+
+  return (
+    <section className="min-h-screen flex flex-col md:flex-row">
+      {/* LEFT SIDE */}
+      <div
+        className="
+        flex flex-col justify-end
+        px-6 sm:px-10 md:px-16 py-10
+        bg-brand-white dark:bg-brand-dark
+        text-brand-dark dark:text-brand-white
+        w-full md:w-1/2"
+      >
+        <div className="leading-none">
+          {/* LOGO */}
+          <figure className="hidden md:flex flex-col items-end mb-3">
+            <img
+              src={theme === "dark" ? darkLogo : LightLogo}
+              alt="The Urban Den logo"
+              className="w-[40px] h-[40px] sm:w-[70px] sm:h-[70px] md:w-[120px] md:h-[120px] object-contain"
+            />
+            <figcaption className="text-[10px] sm:text-xs text-brand-light text-right">
+              The Urban Den
+            </figcaption>
+          </figure>
+
+          <p className="block text-xl sm:text-3xl md:text-5xl font-bold font-heading sm:text-left">
+            THE
+          </p>
+
+          <p className="block text-5xl sm:text-6xl md:text-9xl font-bold font-heading sm:text-left">
+            URBAN
+          </p>
+
+          <p className="block text-5xl sm:text-6xl md:text-9xl font-bold font-heading sm:text-left text-brand-yellow">
+            DEN
+          </p>
+
+          <p className="mt-4 text-[10px] sm:text-xs tracking-widest text-brand-light uppercase sm:text-left">
+            PREMIUM PET GROOMING
+          </p>
+        </div>
+      </div>
+
+      {/* RIGHT IMAGE */}
+      <div className="relative w-full md:w-1/2 h-[60vh] md:h-auto p-2">
+        <img
+          src="https://images.freeimages.com/images/large-previews/bd2/siberian-husky-1464646.jpg?fmt=avif&h=350"
+          alt="Dog"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "../components/Hero";
 
 const LandingPage = () => {
-  return <div>In India coming soon...</div>;
+  return (
+    <section>
+      <Hero />
+    </section>
+  );
 };
 
 export default LandingPage;
